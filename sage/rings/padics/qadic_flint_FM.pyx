ad116f679de93a0494a9bbbf5c136a9ed8023852
David Roe <roed.math@gmail.com>
2013-03-20 20:31:00 -0600

    Add capped absolute and fixed mod, make fraction field coercion work.

--- sage/rings/padics/qadic_flint_FM.pyx
+++ sage/rings/padics/qadic_flint_FM.pyx
@@ -1,5 +1,5 @@
 include "../../libs/linkages/padics/fmpz_poly_unram.pxi"
 include "FM_template.pxi"
 
-cdef class qAdicFixedModulusElement(FMElement):
+cdef class qAdicFixedModElement(FMElement):
     pass
