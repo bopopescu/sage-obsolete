baa7c9219927c1e17a41d7b035f1407b73885b86
David Roe <roed.math@gmail.com>
2013-03-21 19:52:19 -0600

    Added _flint_rep and _flint_rep_abs, fixed printing

--- sage/rings/padics/qadic_flint_FM.pyx
+++ sage/rings/padics/qadic_flint_FM.pyx
@@ -7,4 +7,28 @@ cdef class PowComputer_(PowComputer_flint_unram):
         PowComputer_flint_unram.__init__(self, prime, cache_limit, prec_cap, ram_prec_cap, in_field, poly)
 
 cdef class qAdicFixedModElement(FMElement):
-    pass
+    def _flint_rep(self, var='x'):
+        """
+        Replacement for _ntl_rep for use in printing and debugging.
+
+        EXAMPLES::
+
+            sage: R.<a> = ZqFM(27, 4)
+            sage: (1+a).inverse_of_unit()._flint_rep()
+            41*x^2 + 40*x + 42
+            sage: (1+a)*(41*a^2+40*a+42)
+            1 + O(3^4)
+        """
+        return self.prime_pow._new_fmpz_poly(self.value, var)
+
+    def _flint_rep_abs(self, var='x'):
+        """
+        Replacement for _ntl_rep_abs for use in printing and debugging.
+
+        EXAMPLES::
+
+            sage: R.<a> = ZqFM(27, 4)
+            sage: (3+3*a)._flint_rep_abs()
+            (3*x + 3, 0)
+        """
+        return self._flint_rep(var), Integer(0)
